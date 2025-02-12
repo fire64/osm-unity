e4cce693fa7fb77de78f467a89573fb956496834
Alexey Panin <fire64@yandex.ru>
2025-02-12 22:03:30 +0300

    Add auto calculating size for tiles
    Add auto calculating size for tiles

--- Assets/Scripts/TileSystem.cs
+++ Assets/Scripts/TileSystem.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine.Networking;
+using System;
 
 class TileSystem : InfrstructureBehaviour
 {
     public int zoom = 16; // Zoom level for tiles
     public string tileServerURL = "https://a.tile.openstreetmap.org/{0}/{1}/{2}.png"; // Example tile server URL
+
+    public float GetZoomScale(int zoomLevel)
+    {
+
+        double baseScale = 61.15;
+
+        int referenceLevel = 16;
+
+        return (float)(baseScale / Math.Pow(2, zoomLevel - referenceLevel));
+    }
 
     IEnumerator DownloadTile(int x, int y, int z, Vector3 tilePosition)
     {
@@ -21,11 +32,13 @@ class TileSystem : InfrstructureBehaviour
         }
         else
         {
+            float zoomScale = GetZoomScale(zoom);
+
             Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
             GameObject tileGO = GameObject.CreatePrimitive(PrimitiveType.Plane);
             tileGO.name = $"Tile_{x}_{y}_{z}";
             tileGO.transform.position = tilePosition;
-            tileGO.transform.localScale = new Vector3(61.15f, 1, 61.15f); //TODO: Add calculating for autoresizing...
+            tileGO.transform.localScale = new Vector3(zoomScale, 1, zoomScale); //TODO: Add calculating for autoresizing...
             tileGO.GetComponent<Renderer>().material.mainTexture = texture;
             tileGO.transform.Rotate(0, 180, 0);
         }
